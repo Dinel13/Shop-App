e81717cfdd191912604bcb970be1cf1763a76a30
dinel <solahuddin.hafid.tomaballo@gmail.com>
2020-11-09 19:57:36 +0800

    storing orders

--- store/action/actionOrder.js
+++ store/action/actionOrder.js
@@ -1,8 +1,33 @@
-export const ADD_ORDER ="ADD_ORDER"
+export const ADD_ORDER = "ADD_ORDER";
 
 export const addOrder = (CartItem, totalAmout) => {
-return  {
-    type : ADD_ORDER,
-    orderData  :{items : CartItem, amount : totalAmout}
-}
-}
+  return async (dispatch) => {
+    const date = new Date().toISOString();
+    //code async
+    const response = await fetch(
+      "https://rn-academind-db769.firebaseio.com/orders/u1.json",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ CartItem, totalAmout, date: date }),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("something wrong");
+    }
+    const resData = await response.json();
+
+    dispatch({
+      type: ADD_ORDER,
+      orderData: {
+        id: resData.name,
+        items: CartItem,
+        amount: totalAmout,
+        date: date,
+      },
+    });
+  };
+};
